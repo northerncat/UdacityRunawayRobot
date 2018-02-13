a7157165eb4bbc1231b0c91d32d7b0f2bfc16281
Brad Huang <brad@houzz.com>
2018-02-13 12:26:09 -0800

    Solved part 2 with Extended Kalman Filtering

---
+++ CircularMotionKalmanFilter.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+"""
+An implementation of the Extended Kalman Filter that specializes in estimating
+circular motion variables, including x and y positions, heading direction, and
+turning angle and distance traveled between measurements.
+
+The Extended Kalman Filter (EKF) is the nonlinear version of Kalman Filter that
+can consider nonlinear dynamic systems. In EKF, the transition and observation
+models do not have to be linear, but has to be differentiable. In the case of
+circular motion here, the assumption is valid and thus EKF is applicable.
+"""
+
+from math import cos, sin
+from matrix import matrix
+
+class CircularMotionKalmanFilter(object):
+    """
+    The CircularMotionKalmanFilter class extended the original KalmanFilter by
+    implementing a nonlinear transition model and focusing on using m = 2D
+    position measurements to estimate n = 5D variables, including:
+        1. x position of the robot
+        2. y position of the robot
+        3. the robot's current heading direction
+        4. the robot's turning angle between each measurements
+        5. the distance traveled between each measurements
+    Since the five variables to estimate do not form a linear dynamic system,
+    the transition matrix F is defined to be the Jacobian of the transition
+    model.
+    The estimation update step is the same as the original Kalman Filter since
+    the observation model H remains linear.
+    """
+    def __init__(self):
+        """ Constructor
+        Initializes an Extended Kalman Filter with n=5 variable dimension and
+        m=2 measurement dimension, then set up the H and R matrices.
+        """
+        self.n = 5
+        self.m = 2
+        self.x = matrix([[]])
+        self.x.zero(self.n, 1)
+        self.P = matrix([[]])
+        self.P.zero(self.n, self.n)
+        self.F = matrix([[]])
+        self.F.zero(self.n, self.n)
+        self.u = matrix([[]])
+        self.u.zero(self.n, 1)
+        self.H = matrix([
+            [1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0]
+        ])
+        self.R = matrix([
+            [1.0, 0],
+            [0, 1.0]
+        ])
+        self.I = matrix([[]])
+        self.I.identity(self.n)
+
+    def updateMeasurement(self, z):
+        """ Update estimations of the EKF based on measurements
+        With the input measurements z, updates the x and P matrices.
+
+        Args:
+            z: mx1 matrix, the new measurements
+        """
+        if len(z.value) != self.m or len(z.value[0]) != 1:
+            print 'Invalid dimensions for z, should be (%d, 1)' % self.m
+            return
+        y = z - self.H * self.x
+        S = self.H * self.P * self.H.transpose() + self.R
+        K = self.P * self.H.transpose() * S.inverse()
+        self.x = self.x + K * y
+        self.P = (self.I - K * self.H) * self.P
+
+    def predict(self):
+        """ Update estimations using Kalman Filter as predictions
+        Advnaces in time step and predict the next x and P. Use the current
+        estimations to perform the transition model, and use its Jacobian to
+        update the covariance matrix P.
+
+        Returns:
+            x, y: the predicted x and y positions at the next timestep
+        """
+        F = self.getTransitionMatrix()
+        self.x = self.performTransitionModel()
+        self.P = F * self.P * F.transpose()
+        return self.x.value[0][0], self.x.value[1][0]
+
+    def performTransitionModel(self):
+        """
+        Performs the transition model to get the next state of the variables.
+        Since we assume a circular motion, x and y positions move by the
+        estimated distance along the estimated direction.
+
+        Returns:
+            x: nx1 matrix, estimations of the next state
+        """
+        x = self.getX()
+        y = self.getY()
+        direction = self.getDirection()
+        turnAngle = self.getTurningAngle()
+        dist = self.getDistance()
+        return matrix([
+            [x + dist * cos(direction + turnAngle)],
+            [y + dist * sin(direction + turnAngle)],
+            [direction + turnAngle],
+            [turnAngle],
+            [dist]
+        ])
+
+    def getTransitionMatrix(self):
+        """
+        Generate the transition matrix based on the current estimations. Since
+        the transition model is nonlinear, the transition matrix would be the
+        Jacobian of the transition model. Differentiating the model against the
+        variables gives us the matrix as shown below.
+
+        Returns:
+            F: nxn matrix, transition matrix / Jacobian of the transition model
+        """
+        d = self.getDistance()
+        angle = self.getDirection() + self.getTurningAngle()
+        return matrix([
+            [1, 0, -d*sin(angle), -d*sin(angle), cos(angle)],
+            [0, 1, d*cos(angle), d*cos(angle), sin(angle)],
+            [0, 0, 1, 1, 0],
+            [0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 1]
+        ])
+
+    def getX(self):
+        """ Get the current estimation of the x position. """
+        return self.x.value[0][0]
+
+    def getY(self):
+        """ Get the current estimation of the y position. """
+        return self.x.value[1][0]
+
+    def getDirection(self):
+        """
+        Get the current estimation of the heading direction in radians.
+        """
+        return self.x.value[2][0]
+
+    def getTurningAngle(self):
+        """ Get the current estimation of the turning angle. """
+        return self.x.value[3][0]
+
+    def getDistance(self):
+        """ Get the current estimation of the distance traveled. """
+        return self.x.value[4][0]
--- part2_noisy_measurements.py
+++ part2_noisy_measurements.py
@@ -36,111 +36,46 @@ from matrix import matrix # Check the matrix.py tab to see how this works.
 # next position. The OTHER variable that your function returns will be
 # passed back to your function the next time it is called. You can use
 # this to keep track of important information over time.
-from KalmanFilter import KalmanFilter
+from CircularMotionKalmanFilter import CircularMotionKalmanFilter
 
 UNCERTAINTY_COVARIANCE = 1000.0
-MEASUREMENT_NOISE = 10.0
 
-def initializeKalmanFilter(m):
-    kf = KalmanFilter(3, m)
+def initializeKalmanFilter():
+    kf = CircularMotionKalmanFilter()
     # P would be diagonal with the large constant uncertainty covariance
     kf.P = matrix([
-        [UNCERTAINTY_COVARIANCE, 0, 0],
-        [0, UNCERTAINTY_COVARIANCE, 0],
-        [0, 0, UNCERTAINTY_COVARIANCE]
-    ])
-    # distance and turning angle should be constant, while the heading
-    # direction keeps increasing by the turning angle
-    kf.F = matrix([
-        [1, 0, 0],
-        [0, 1, 1],
-        [0, 0, 1]
-    ])
-    # the measurement matrix would be the distance and the direction
-    kf.H = matrix([
-        [1, 0, 0],
-        [0, 1, 0]
-    ])
-    # the measurement noise of the distance and direction is unknown,
-    # so I simply set it to the identity matrix...
-    # TODO: find a better estimate of the measurement noise!
-    kf.R = matrix([
-        [1, 0],
-        [0, 1]
+        [UNCERTAINTY_COVARIANCE, 0, 0, 0, 0],
+        [0, UNCERTAINTY_COVARIANCE, 0, 0, 0],
+        [0, 0, UNCERTAINTY_COVARIANCE, 0, 0],
+        [0, 0, 0, UNCERTAINTY_COVARIANCE, 0],
+        [0, 0, 0, 0, UNCERTAINTY_COVARIANCE]
     ])
     return kf
-
-def getDistAngle(measurement, prevMeasurement, prevEstimate):
-    dx = measurement[0] - prevMeasurement[0]
-    dy = measurement[1] - prevMeasurement[1]
-    dist = sqrt(dx ** 2 + dy ** 2)
-    angle = atan2(dy, dx)
-    if prevEstimate:
-        prevAngle = prevEstimate.value[1][0]
-        prevTurn = prevEstimate.value[2][0]
-        if prevTurn > 0 and angle < prevAngle:
-            while angle < prevAngle:
-                angle += 2 * pi
-        elif prevTurn < 0 and angle > prevAngle:
-            while angle > prevAngle:
-                angle -= 2 * pi
-    return dist, angle
-
 
 def estimate_next_pos(measurement, OTHER=None):
     """Estimate the next (x, y) position of the wandering Traxbot
     based on noisy (x, y) measurements.
 
-    I use a Kalman Filter to estimate three parameters:
-        1. the distance the robot travels between measurements
-        2. the current heading direction of the robot
-        3. the angle the robot turns between measurements
-    This allows the transition matrix to be easily constructed, as shown below.
+    I use an Extended Kalman Filter (EKF) to estimate five parameters:
+        1. the x position
+        2. the y position
+        3. the current heading direction of the robot
+        4. the angle the robot turns between measurements
+        5. the distance the robot travels between measurements
     """
 
-    # default the prediction to be the same position as measurement
-    x, y = measurement
-
-    # If the Kalman Filter has not been initialized, create an instance.
+    # create an instance of the EKF
     if not OTHER:
-        # initializes OTHER to store the Kalman Filter and the measurements
-        OTHER = {}
-        OTHER['KalmanFilter'] = initializeKalmanFilter(len(measurement))
-        OTHER['prevMeasurement'] = False
-        OTHER['prevEstimate'] = False
-        OTHER['pathLength'] = 0
+        OTHER = initializeKalmanFilter()
 
-    OTHER['pathLength'] += 1
+    # set up the measurement, updates the EKF and predict the next step
+    z = matrix([
+        [measurement[0]],
+        [measurement[1]]
+    ])
+    OTHER.updateMeasurement(z)
+    x, y = OTHER.predict()
 
-    if not OTHER['prevMeasurement']:
-        # cannot make good predictions using only one measurement, so just
-        # record the measurement in the path
-        pass
-    elif OTHER['pathLength'] == 2:
-        # initialize the estimates in the Kalman Filter after two measurements
-        dist, angle = getDistAngle(measurement, OTHER['prevMeasurement'], OTHER['prevEstimate'])
-        OTHER['KalmanFilter'].x = matrix([
-            [dist],
-            [angle],
-            [0.0]
-        ])
-    else:
-        # with more than two measurements, start updating the Kalman filter
-        # with the new measurements
-        dist, angle = getDistAngle(measurement, OTHER['prevMeasurement'], OTHER['prevEstimate'])
-        z = matrix([
-            [dist],
-            [angle]
-        ])
-        OTHER['KalmanFilter'].updateMeasurement(z)
-        estimates, _ = OTHER['KalmanFilter'].predict()
-        estimateDist = estimates.value[0][0]
-        estimateAngle = estimates.value[1][0]
-        x = measurement[0] + estimateDist * cos(estimateAngle)
-        y = measurement[1] + estimateDist * sin(estimateAngle)
-        OTHER['prevEstimate'] = estimates
-
-    OTHER['prevMeasurement'] = measurement
     return (x, y), OTHER
 
 # A helper function you may find useful.
@@ -156,6 +91,7 @@ def demo_grading(estimate_next_pos_fcn, target_bot, OTHER=None):
     This is here to give you a sense for how we will be running and grading
     your code. Note that the OTHER variable allows you to store any
     information that you want.
+    """
     """
     localized = False
     distance_tolerance = 0.01 * target_bot.distance
@@ -211,6 +147,28 @@ def demo_grading(estimate_next_pos_fcn, target_bot, OTHER=None):
         prediction.stamp()
         #End of Visualization
     return localized
+    """
+
+    localized = False
+    distance_tolerance = 0.01 * target_bot.distance
+    ctr = 0
+    # if you haven't localized the target bot, make a guess about the next
+    # position, then we move the bot and compare your guess to the true
+    # next position. When you are close enough, we stop checking.
+    while not localized and ctr <= 1000:
+        ctr += 1
+        measurement = target_bot.sense()
+        position_guess, OTHER = estimate_next_pos_fcn(measurement, OTHER)
+        target_bot.move_in_circle()
+        true_position = (target_bot.x, target_bot.y)
+        error = distance_between(position_guess, true_position)
+        if error <= distance_tolerance:
+            # print "You got it right! It took you ", ctr, " steps to localize."
+            localized = True
+        if ctr == 1000:
+            # print "Sorry, it took you too many steps to localize the target."
+            return localized, ctr
+    return localized, ctr
 
 # This is a demo for what a strategy could look like. This one isn't very good.
 def naive_next_pos(measurement, OTHER=None):
@@ -222,10 +180,23 @@ def naive_next_pos(measurement, OTHER=None):
     xy_estimate = OTHER
     return xy_estimate, OTHER
 
-# This is how we create a target bot. Check the robot.py file to understand
-# How the robot class behaves.
-test_target = robot(2.1, 4.3, 0.5, 2*pi / 34.0, 1.5)
-measurement_noise = 0.05 * test_target.distance
-test_target.set_noise(0.0, 0.0, measurement_noise)
-
-demo_grading(estimate_next_pos, test_target)
+# run multiple trials of the program to get a good sense of its capability
+NUM_TRIALS = 100
+localized = 0
+counter = []
+for i in range(NUM_TRIALS):
+    # create a test target
+    test_target = robot(2.1, 4.3, 0.5, 2*pi / 34.0, 1.5)
+    measurement_noise = 0.2 * test_target.distance
+    test_target.set_noise(0.0, 0.0, measurement_noise)
+    # run the estimation function to decided if the target is found
+    found, ctr = demo_grading(estimate_next_pos, test_target)
+    # record the successes!
+    if found:
+        localized += 1
+        counter.append(ctr)
+# print out the final results with success rate and the average iteration number
+successPercent = 100.0 * localized / NUM_TRIALS
+avgCounter = sum(counter) / len(counter)
+print 'Localized {:.2f}% of the trials'.format(successPercent)
+print 'Averaged {:.1f} iterations.'.format(avgCounter)
